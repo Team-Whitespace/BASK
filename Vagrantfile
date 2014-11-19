e3588df057c288d9dfe99d9365056ad9f83faeaf
Kiran J Hampal <kiran@elementaryos.org>
2014-11-19 01:37:33 +0000

    Solr 4.10 works, and can be accessed via the host machine

--- Vagrantfile
+++ Vagrantfile
@@ -5,5 +5,6 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "hashicorp/precise64"
+  config.vm.network :forwarded_port, guest: 8080, host: 8080, host_ip: "127.0.0.1", auto_correct: true # Solr
   config.vm.provision :shell, path: "vagrant/setup.sh"
 end
--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -1,31 +1,47 @@
 #!/usr/bin/env bash
 
 kafka_mirror=http://mirror.ox.ac.uk/sites/rsync.apache.org/kafka/0.8.1.1/kafka_2.9.1-0.8.1.1.tgz
+solr_mirror=http://mirror.ox.ac.uk/sites/rsync.apache.org/lucene/solr/4.10.2/solr-4.10.2.tgz
 
 setup_kafka () {
     wget --quiet -O kafka.tgz $kafka_mirror > /dev/null
     tar xvf kafka.tgz > /dev/null
     rm -f kafka.tgz
-    mv kafka_*/ /usr/local/kafka/
-    echo "export PATH=$PATH:/usr/local/kafka/bin" >> .bashrc
+    mv kafka_*/ kafka/
+}
+
+setup_solr () {
+    wget --quiet -O solr.tgz $solr_mirror > /dev/null
+    tar xvf solr.tgz > /dev/null
+    rm -f solr.tgz
+    mv solr-*/ solr/
+    cp solr/example/lib/ext/* /usr/share/tomcat7/lib/
+    cp solr/dist/solr-*.war /var/lib/tomcat7/webapps/solr.war
+    cp -R solr/example/solr /var/lib/tomcat7
+    chown -R tomcat7:tomcat7 /var/lib/tomcat7/solr
+    cp /vagrant/vagrant/config/tomcat-users.xml /etc/tomcat7/tomcat-users.xml
+    service tomcat7 restart
 }
 
 setup_baleene () {
-    echo "Setting up Baleene"
+    echo "TODO"
 }
 
 echo "Updating repositories"
 apt-get -y update > /dev/null
 
-echo "Installing software from repositories"
+echo "Installing software from repositories [this may take a while]"
 apt-get -y install openjdk-7-jre-headless openjdk-7-jdk \
-                   solr-jetty jetty libjetty-extra scala \
+                   tomcat7 tomcat7-admin scala \
                    maven icedtea-7-plugin \
                    python-software-properties vim git-core curl \
                    > /dev/null
 
 echo "Updating alternatives to Java 7"
 update-java-alternatives -s java-1.7.0-openjdk-amd64
+
+echo "Setting up Solr [this may take a while]"
+setup_solr
 
 echo "Setting up Kafka"
 setup_kafka
