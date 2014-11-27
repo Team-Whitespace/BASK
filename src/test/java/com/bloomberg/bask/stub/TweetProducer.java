afec5ac19cbd45f5c3bdb43f099a55f6527082ae
Kiran J Hampal <kiran@elementaryos.org>
2014-11-27 03:18:56 +0000

    Added sample tweets, queries and the start of a tweet producer stub

---
+++ src/test/java/com/bloomberg/bask/stub/TweetProducer.java
@@ -0,0 +1,8 @@
+package com.bloomberg.bask.stub;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+public class TweetProducer {
+}
