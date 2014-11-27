9a7c51d3e7ac26c8c0a3c6ec0992500b1e8762f8
Jack T Markham <jm13559@my.bristol.ac.uk>
2014-11-27 11:37:13 +0000

    Fixed build script to download custom Lucene stuff (maven didn't always build correctly)

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+echo "Cloning and building Lucene intervals"
+git clone -b lucene_solr_4_8 https://github.com/bloomberg/lucene-solr.git
+cd lucene-solr/maven-build
+git reset --hard HEAD; git clean -f -d; git pull
+mvn clean install -DskipTests
+cd ../..
+
 echo "Cloning and building Luwak"
 git clone -b 1.1.x https://github.com/bloomberg/luwak.git
 cd luwak
